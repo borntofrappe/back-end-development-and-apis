aba6c71af176123984e4d9ce7307ebfa88f6d49e
borntofrappe <borntofrappe@gmail.com>
2022-01-01 17:03:54 +0100

    complete project without a database

--- url-shortener-microservice/README.md
+++ url-shortener-microservice/README.md
@@ -8,4 +8,17 @@ Create an application similar [to the example URL Shortener Microservice](https:
 
 - [REPL](https://replit.com/@borntofrappe/boilerplate-project-urlshortener)
 
-<!-- ## Notes -->
+## Assignment
+
+> You can POST a URL to `/api/shorturl` and get a JSON response with `original_url` and `short_url` properties
+
+    ```json
+    {
+      "original_url": "https://freeCodeCamp.org",
+      "short_url": 1
+    }
+    ```
+
+> When you visit `/api/shorturl/<short_url>`, you will be redirected to the original URL
+
+> If you pass an invalid URL that doesn't follow the valid format, the JSON response will contain `{ "error": "invalid url" }`
---
+++ url-shortener-microservice/server.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const dns = require("dns");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+let shorturls = [];
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// Your first API endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// POST /api/shorturl
+// retrieve url_input from body of the request
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url || "";
+  const hostname = url.replace(/http[s]?\:\/\//, "").replace(/\/(.+)?/, "");
+
+  if (hostname) {
+    dns.lookup(hostname, (err) => {
+      if (err) {
+        res.json({
+          error: "Invalid URL",
+        });
+      } else {
+        const shorturl = shorturls.find((d) => d.original_url === url);
+        if (shorturl) {
+          const { original_url, short_url } = shorturl;
+          res.json({
+            original_url,
+            short_url,
+          });
+        } else {
+          const short_url = shorturls.length;
+          const shorturl = {
+            original_url: url,
+            short_url,
+          };
+          shorturls.push(shorturl);
+          res.json(shorturl);
+        }
+      }
+    });
+  } else {
+    res.json({
+      error: "Invalid URL",
+    });
+  }
+});
+
+// GET /api/shorturl/<shorturl>
+// <shorturl>: integer
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const short_url = parseInt(req.params.short_url, 10);
+  if (short_url !== NaN) {
+    const shorturl = shorturls.find((d) => d.short_url === short_url);
+
+    if (shorturl) {
+      res.redirect(shorturl.original_url);
+    } else {
+      res.json({
+        error: "No short URL found for the given input",
+      });
+    }
+  } else {
+    res.json({
+      error: "Wrong format",
+    });
+  }
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
